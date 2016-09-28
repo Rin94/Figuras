e02bf8f95cf37385ebfc6891bd7508ee089100a6
Jared Raku <vagrant@vagrant.vagrant-debian-amd64>
2016-09-28 11:04:43 -0500

    Anade funcionalidad para la figura al cuadrado
    Se agrego la funcionalidad para obtener el area de un cuadrado de la misma
    forma se agrego las pruebas unitarias para la funcionalidad

---
+++ Figuras.py
@@ -0,0 +1,12 @@
+__author__ = 'Jerad'
+import math
+class Figuras:
+    def cuadrado(self,lado):
+        try:
+            lado=float(lado)
+            math.pi
+            return lado*lado
+
+
+        except Exception, e:
+            return "Dato Incorrecto"
---
+++ UniTest.py
@@ -0,0 +1,30 @@
+__author__ = 'Jerad'
+import unittest
+from Figuras import Figuras
+
+class TestFiguras(unittest.TestCase):
+
+    def setUp(self):
+        self.figura=Figuras()
+
+    def test_area_cuadrado_lado_5(self):
+        resultado = self.figura.cuadrado(5)
+        self.assertEqual(25,resultado)
+
+    def test_area_cuadrado_lado_g(self):
+        resultado = self.figura.cuadrado("g")
+        self.assertEqual("Dato Incorrecto",resultado)
+
+    def test_area_cuadrado_lado_4(self):
+        resultado = self.figura.cuadrado(4.5)
+        self.assertEqual(20.25,resultado)
+
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
